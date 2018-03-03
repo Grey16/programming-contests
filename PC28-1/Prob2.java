285b5b2fd7215b16c95144b40839fac36604808a
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-03-02 19:00:21 -0500

    Create Prob2.java

---
+++ PC28-1/Prob2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Prob2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            int x = in.nextInt();
+            int n = in.nextInt();
+            if(x == 0 && n == 0) {
+                break;
+            }
+            System.out.println("The value is " + function(x, n));
+        }
+    }
+    public static int function(int x, int n) {
+        int lastTerm = x;
+        int current = 0;
+        for(int i = 2; i <= n; i++) {
+            if(i % 2 == 0) {
+                current = i - lastTerm;
+            } else {
+                current = i + lastTerm;
+            }
+            lastTerm = current;
+        }
+        return current;
+    }
+}
