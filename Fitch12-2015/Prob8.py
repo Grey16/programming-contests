b054bbe53401bfa6246d74c0be2133d7e5083359
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-04-16 20:58:02 -0400

    Update Prob8.py

--- Fitch12-2015/Prob8.py
+++ Fitch12-2015/Prob8.py
@@ -1,1 +1,17 @@
+import math
+
 def main():
+    for i in range(4):
+        arr = list(map(float, input().strip().split(" ")))
+        a = arr[0]
+        b = arr[1]
+        c = arr[2]
+        if a < b:
+            temp = a
+            a = b
+            b = temp
+        radian = math.asin((a - b) / c)
+        answer = math.pi * (a + b) + 2 * (radian * a) - 2 * (radian * b) + 2 * (math.sqrt(c ** 2 - (a - b) ** 2))
+        print(answer)
+
+if __name__ == "__main__": main()
