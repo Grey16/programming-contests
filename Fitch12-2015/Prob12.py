15692ed0b1bd04e1b5616660e1f04796285e8764
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-04-22 09:52:54 -0400

    Update Prob12.py

--- Fitch12-2015/Prob12.py
+++ Fitch12-2015/Prob12.py
@@ -1,8 +1,11 @@
 def main():
     for i in range(1):
-        matrix = [[0] * 9 for i in range(9)]
-        for i in range(9):
-            matrix[i] = list(input())
+        matrix = [[0] * 9 for j in range(9)]
+        for j in range(9):
+            matrix[j] = list(input())
+        for row in range(9):
+            for col in range(9):
+                matrix[row][col] = int(matrix[row][col])
         if isBroken(matrix):
             print("already broken")
         else:
@@ -11,18 +14,44 @@ def main():
 def solve(matrix):
     total = 0
     if isBroken(matrix):
+        # print("This is broken")
         return -1
+    elif isFinished(matrix):
+        print("Matrix is finished.")
+        return 1
     else:
         for row in range(9):
             for col in range(9):
                 if matrix[row][col] == 0:
                     for i in range(9):
                         matrix[row][col] = i + 1
+                        # print("Assigned: " + str(matrix[row][col]) + " to row " + str(row) + " col " + str(col))
                         if solve(matrix) != -1:
                             total += 1
+                            print("Increment total")
+                            return total
                         else:
+                            if matrix[row][col] == 9:
+                                matrix[row][col] = 0
+                                print("Ran out of options")
+                                print("Row: " + str(row) + " Col: " + str(col))
+                                if row == 8 and col == 8:
+                                    for row in matrix:
+                                        print(row)
+                                return -1
                             matrix[row][col] = 0
-        return total
+        if total != 0:
+            return total
+        else:
+            print("Could not solve")
+            return -1
+
+def isFinished(matrix):
+    for i in range(9):
+        for j in range(9):
+            if matrix[i][j] == 0:
+                return False
+    return True
 
 def isBroken(matrix):
     for i in range(9):
