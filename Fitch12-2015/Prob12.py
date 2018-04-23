d6dc284eea2166fd9d3820bbdf0f60b0b92a02bf
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-04-23 17:05:27 -0400

    Update Prob12.py

--- Fitch12-2015/Prob12.py
+++ Fitch12-2015/Prob12.py
@@ -12,12 +12,10 @@ def main():
             print(solve(matrix))
 
 def solve(matrix):
-    total = 0
+    result = 0
     if isBroken(matrix):
-        # print("This is broken")
         return -1
     elif isFinished(matrix):
-        print("Matrix is finished.")
         return 1
     else:
         for row in range(9):
@@ -25,26 +23,20 @@ def solve(matrix):
                 if matrix[row][col] == 0:
                     for i in range(9):
                         matrix[row][col] = i + 1
-                        # print("Assigned: " + str(matrix[row][col]) + " to row " + str(row) + " col " + str(col))
-                        if solve(matrix) != -1:
-                            total += 1
-                            print("Increment total")
-                            return total
-                        else:
-                            if matrix[row][col] == 9:
-                                matrix[row][col] = 0
-                                print("Ran out of options")
-                                print("Row: " + str(row) + " Col: " + str(col))
-                                if row == 8 and col == 8:
-                                    for row in matrix:
-                                        print(row)
-                                return -1
+                        if isFinished(matrix) and not isBroken(matrix):
+                            for row in matrix:
+                                print(row)
+                            return 1
+                        if solve(matrix) >= 1:
+                            result += 1
+                        if matrix[row][col] == 9:
                             matrix[row][col] = 0
-        if total != 0:
-            return total
-        else:
-            print("Could not solve")
-            return -1
+                            # print("Ran out of options")
+                            # print("Row: " + str(row) + " Col: " + str(col))
+                            if result == 0:
+                                return 0
+                        matrix[row][col] = 0
+        return result
 
 def isFinished(matrix):
     for i in range(9):
@@ -112,3 +104,23 @@ if __name__ == "__main__": main()
 # 036000080
 # 000050103
 # 005680200
+
+# 054038900
+# 109040000
+# 020000710
+# 000306001
+# 790000046
+# 800704000
+# 036000080
+# 000050103
+# 005680200
+
+# 004038900
+# 109040000
+# 020000710
+# 000306001
+# 790000046
+# 800704000
+# 036000080
+# 000050103
+# 005680000
